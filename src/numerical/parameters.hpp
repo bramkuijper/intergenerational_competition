8d888d4c7e9298f5f0a42c5f74d7fc7b808a1e0c
Bram Kuijper <bramkuijper@gmail.com>
2024-01-04 08:51:40 +0000

    now implementing cost functions

--- src/numerical/parameters.hpp
+++ src/numerical/parameters.hpp
@@ -1,10 +1,12 @@
 #ifndef _PARAMETERS_HPP_
 #define _PARAMETERS_HPP_
 
-enum Sex
+#include <string>
+
+enum ModelType
 {
-    male=0,
-    female=1
+    AD=0, // adult defence - adults can only defend their own breeding position
+    FS=1 // adults and juveniles compete anew every time step over breeding positions
 };
 
 class Parameters
@@ -12,8 +14,30 @@ class Parameters
     public:
 
         // juvenile dispersal 
-        double d[2]{0.5,0.05};
+        double d{0.5};
 
+        // number of adult breeders in a patch
+        unsigned n{3};
+
+        // adult survival rate
+        double m_baseline{0.2};
+
+        std::string file_name{"iter_intergenerational"};
+
+        // maximum amount of time before we give up on 
+        // converging relatedness coefficients
+        unsigned long max_eco_time{10000};
+
+        // maximum amount of time before we give up on 
+        // converging evolving variables
+        unsigned long max_evo_time{90000};
+
+        // initial value for survival help
+        // before we let the trait evolve
+        double survival_help_init{0.0};
+        // initial value for fecundity help
+        // before we let the trait evolve
+        double fecundity_help_init{0.0};
 };
 
 #endif 
